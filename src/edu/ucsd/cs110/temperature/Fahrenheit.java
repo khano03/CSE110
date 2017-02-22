58e3db7ddb7e985100bcedc1bda6e16f0eb310b5
Jonathan Chang <jbchang@ucsd.edu>
2017-02-22 00:30:05 -0800

    add classpath

---
+++ src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -0,0 +1,26 @@
+package edu.ucsd.cs110.temperature;
+
+/**
+ * Created by user on 2/15/2017.
+ */
+public class Fahrenheit extends Temperature {
+    public Fahrenheit(float t) {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        return null;
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return null;
+    }
+
+    public String toString()
+    {
+        //TODO: Complete this method
+        return "";
+    }
+}
